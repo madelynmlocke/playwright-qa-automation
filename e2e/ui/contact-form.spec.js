c8a5c1e92cdc69458631b2ecb99865bad23f3f8d
madelynmleahu <50344146+madelynmleahu@users.noreply.github.com>
2026-04-01 13:23:04 -0700

    added scroll functionality test
    integration / regression testing

--- e2e/ui/contact-form.spec.js
+++ e2e/ui/contact-form.spec.js
@@ -1,13 +1,13 @@
 import { test, expect } from '../../fixtures/index.js';
 
 test.describe('@ui @contactform Contact Form Validation Tests', () => {
-    test('Test Case 6: Contact form fields are visible and user submits form @smoke', async ({ contactPage }) => {
+    test('Test Case 6: Contact form fields are visible and user submits form @smoke', async ({ contactPage }) => {  
         await contactPage.goToHomePage();
         await contactPage.gotoContactPage();
         await contactPage.assertForm();
         await contactPage.fillForm('maddy', process.env.EMAIL_VALID, 'test', 'testing a message');
         await contactPage.handleDialogOnce();
         await contactPage.submitForm();
-        await contactPage.assertSubmission();
+        await contactPage.assertSubmission();  
     });
 });
--- e2e/ui/homepage.spec.js
+++ e2e/ui/homepage.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '../../fixtures/index.js';
 
 test.describe('@ui @home Homepage Tests', () => {
     
-    test('@smoke test verifies key content...', async ({ homePage }) => {
+    test('@smoke test verifies key content', async ({ homePage }) => {    
         await homePage.gotoHomePage();
         await homePage.assertLayout();
         await homePage.assertLogo();
@@ -10,8 +10,7 @@ test.describe('@ui @home Homepage Tests', () => {
         await homePage.assertNavLinksVisible();
     });
 
-    test('user can interact with slideshow and change screens', async ({ homePage }) => {
-
+    test('user can interact with slideshow and change screens @regression', async ({ homePage }) => {
         await homePage.gotoHomePage();
         await homePage.assertFirstSlide();
         await homePage.nextSlide();
@@ -21,10 +20,16 @@ test.describe('@ui @home Homepage Tests', () => {
         await homePage.nextSlide();
     });
 
-    test('user can redirect to pages with navigation', async ({ homePage }) => {
-
+    test('user can redirect to pages with navigation @regression', async ({ homePage }) => {
         await homePage.gotoHomePage();
         await homePage.assertNavLinksNavigate();
+    });
 
+    test('verify scroll up and scroll down functionality using arrow button @integration @regression', async ({ homePage, page}) => {
+        await homePage.gotoHomePage();
+        await homePage.scrollDown(page);
+        await homePage.scrollWithPageDown(page);
+        await homePage.assertSub();
+        await homePage.scrollWithButton();
     });
 });
